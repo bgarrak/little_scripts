2d8f0ef911fa219257c610fb49d0dc7e511aebb1
bgarrak <bgarrak@gmail.com>
2021-07-21 20:07:15 -0700

    Comments & spacing

--- flatten_structure.ps1
+++ flatten_structure.ps1
@@ -1,5 +1,9 @@
+# Purpose: Recursively copy all objects within a folder, and paste within another folder
 
+# Source
 [string] $SourceDirectoryPath = 'C:\'
+
+# Destination
 [string] $TargetDirectoryPath = 'C:\'
 
 
@@ -7,8 +11,5 @@ New-Item $TargetDirectoryPath -type directory
 Get-ChildItem $SourceDirectoryPath -recurse |
     Where-Object { $_.PSIsContainer -eq $False } | ` #Remove the empty folders here
     ForEach-Object {Copy-Item -Path $_.Fullname -Destination $TargetDirectoryPath -Force}
-
-
-
 
 
